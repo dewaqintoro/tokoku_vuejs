ec5f9ad1cdcca834330d93849d6decf35261d5c3
dewaqintoro <dewa.qintoro@students.amikom.ac.id>
2020-10-21 01:35:41 +0700

    p3

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2>Barang Elektronik <strong><br>Berkualitas</strong></h2>
+          <p>Temukan gadget impianmu.</p>
+          <button class="btn btn-lg btn-success">Pesan <b-icon-arrow-right></b-icon-arrow-right></button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/images/Hero.png" width="100%"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Elektronik.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar/>
+    <h2>Daftar Elektronik</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+
+export default {
+  name:"Elektronik",
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
