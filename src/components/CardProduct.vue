99866624b5c20a1e0e256513ded8e440dcaff1d8
dewaqintoro <dewa.qintoro@students.amikom.ac.id>
2020-10-21 19:03:53 +0700

    p5

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,21 @@
+<template> 
+  <div class="card shadow card-product">
+    <img class="card-img-top" :src="'assets/images/'+product.gambar" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">{{product.nama}}</h5>
+      <p class="card-text">Harga : Rp. {{product.harga}}</p>
+      <a href="#" class="btn btn-success"><b-icon-cart></b-icon-cart> Pesan</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"CardProduct",
+    props:['product']
+  }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,21 @@
     <Navbar/>
     <div class="container">
       <Hero/>
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Produk <strong>Terlaris</strong></h2>
+        </div>
+
+        <div class="col">
+          <router-link to="/elektronik" class="btn btn-success float-right"><b-icon-eye></b-icon-eye> Selengkapnya</router-link>
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <div class="col-md mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,13 +26,36 @@
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
-
+import CardProduct from '@/components/CardProduct.vue'
+import axios from "axios"
 
 export default {
   name: 'Home',
   components: {
     Navbar,
-    Hero
+    Hero,
+    CardProduct
+  },
+  data(){
+    return{
+      products:[]
+    }
+  },
+  methods:{
+    setProduct(data){
+      this.products = data;
+    }
+  },
+  mounted(){
+    axios
+      .get("http://localhost:3004/best-products")
+      .then((response) => (
+        this.setProduct(response.data))
+      )
+      .catch((error) => (
+        console.log("gagal",error)
+        )
+      )
   }
 }
 </script>
